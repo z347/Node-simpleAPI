e27350c9a3af59f69b6e938a64c53442abc7449c
z347 <shulo.vova@gmail.com>
2020-10-14 14:43:27 +0300

    working settings for development mode

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,11 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('test');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running in http://localhost:${PORT}`);
+    console.log(`⚡️⚡️⚡️ Server is running in http://localhost:${PORT} ⚡️⚡️⚡️`);
 });
